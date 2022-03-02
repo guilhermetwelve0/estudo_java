bfb81ee993243dc1e08edea15f537fdf98fe8cf2
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2022-03-02 18:03:14 -0300

    Projeto_java

--- src/classe/AreaCircTeste.java
+++ src/classe/AreaCircTeste.java
@@ -1,5 +1,21 @@
 package classe;
 
 public class AreaCircTeste {
-
+   
+	public static void main(String[] args) {
+		
+		AreaCirc a1 = new AreaCirc(10);
+		//a1.pi = 1000000;
+		
+		AreaCirc a2 = new AreaCirc(5);
+		//a2.pi = 0;
+		
+		AreaCirc.pi = 3.1415;
+		
+		
+		System.out.println(a1.area());
+		
+		AreaCirc.pi = 0.1;
+		System.out.println(a2.area());
+	}
 }
