ff25752fcab832edf8103ccc081ffbe1b59a6761
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2022-03-03 15:17:33 -0300

    initial commit

--- src/classe/AreaCircTeste.java
+++ src/classe/AreaCircTeste.java
@@ -5,17 +5,18 @@ public class AreaCircTeste {
 	public static void main(String[] args) {
 		
 		AreaCirc a1 = new AreaCirc(10);
+		a1.raio = 10;
 		//a1.pi = 1000000;
 		
-		AreaCirc a2 = new AreaCirc(5);
-		//a2.pi = 0;
 		
-		AreaCirc.pi = 3.1415;
+		
+		
 		
 		
 		System.out.println(a1.area());
 		
-		AreaCirc.pi = 0.1;
-		System.out.println(a2.area());
+		System.out.println(AreaCirc.area(100));
+		System.out.println(AreaCirc.PI);
+		System.out.println(Math.PI);
 	}
 }
